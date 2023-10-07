ec0cc403818a29a4527c88398f9f835fca021dba
if <55980773+Yifeeeeei@users.noreply.github.com>
2023-10-07 11:22:37 -0700

    better json version control

---
+++ mass_producer/generate_default_jsons.py
@@ -0,0 +1,98 @@
+from json_version_control import JsonVersionController
+import json
+
+mass_producer_dict = {
+    "生物": {
+        "skip": False,
+        "xlsx_paths": ["sheets/全卡表-生物.xlsx"],
+        "drawing_paths": ["牌面图片/生物"],
+    },
+    "技能": {
+        "skip": False,
+        "xlsx_paths": ["sheets/全卡表-技能.xlsx"],
+        "drawing_paths": ["牌面图片/技能"],
+    },
+    "道具": {
+        "skip": False,
+        "xlsx_paths": ["sheets/全卡表-道具.xlsx"],
+        "drawing_paths": ["牌面图片/道具"],
+    },
+    "英雄": {
+        "skip": False,
+        "xlsx_paths": ["sheets/全卡表-英雄.xlsx"],
+        "drawing_paths": ["牌面图片/英雄"],
+    },
+    "尺寸": 2,
+    "打印版": False,
+    "@排版": "游戏王or万智牌",
+    "排版": "游戏王",
+    "overwrite": True,
+    "mixedup_elements": True,
+    "output_path": "output",
+    "general_path": "resources/general",
+    "font_path": "resources/fonts",
+}
+
+pack_maker_dict = {
+    "row_num": 5,
+    "@row_num": "一共几行",
+    "col_num": 6,
+    "@col_num": "一共几列",
+    "all_cards_dir": "./output",
+    "@all_cards_dir": "卡牌的文件夹，dir/类别/属性/卡牌",
+    "overwrite": True,
+    "make_all_cards": True,
+    "@make_all_cards": "是否生成所有卡牌，在生成所有牌以后快速将所有的牌生成到一张图片上，导入tts",
+    "mixedup_elements": True,
+    "@mixedup_elements": "是否将所有的元素混合在一起，只保留类别文件夹，如果为false，则会按照元素分别生成卡牌",
+    "all_cards_output_dir": "./all_cards_output",
+    "make_single_deck": True,
+    "@make_single_deck": "是否生成单个卡组,如果true，则会按照提供的文件生成单个卡组",
+    "deck_txt_path": "./卡组/earth.txt",
+    "@deck_txt_path": "卡组文件的路径，每一行是一张卡牌的路径，如果是相对路径，则是相对于cards_dir的路径，txt每一行为一个卡牌号",
+    "single_deck_output_dir": "./deck",
+}
+single_card_maker_dict = {
+    "编号": "100001",
+    "名称": "大法师 伦德萨尔",
+    "类别": "生物",
+    "属性": "光",
+    "标签": "巫师",
+    "引言": "“真是没完没了，幸好我还剩下一招，一个真正的绝活！”",
+    "条件": "6光",
+    "效果": "入场，遗言：使你的一个法术获得+4威力，+1重伤",
+    "负载": "2光",
+    "生命": 2,
+    "持续时间": 0,
+    "威力": 0,
+    "代价": "",
+    "尺寸": 2,
+    "原图文件夹": ".",
+    "输出文件夹": ".",
+    "打印版": False,
+    "@排版": "游戏王or万智牌",
+    "排版": "游戏王",
+}
+
+if __name__ == "__main__":
+    json_path = "mass_producer_params_xlsx.json"
+    json.dump(
+        mass_producer_dict,
+        open(json_path, "w", encoding="utf-8"),
+        ensure_ascii=False,
+        indent=4,
+    )
+    json_path = "pack_maker_params.json"
+    json.dump(
+        pack_maker_dict,
+        open(json_path, "w", encoding="utf-8"),
+        ensure_ascii=False,
+        indent=4,
+    )
+    json_path = "single_card_maker_params.json"
+    json.dump(
+        single_card_maker_dict,
+        open(json_path, "w", encoding="utf-8"),
+        ensure_ascii=False,
+        indent=4,
+    )
--- mass_producer/handbook_for_mass_production.md
+++ mass_producer/handbook_for_mass_production.md
@@ -1,10 +1,18 @@
 # 如何使用
 
-## 1 安装依赖
+## 1 安装依赖并生成配置文件
 
 ```bash
 pip install -r requirements.txt
 ```
+
+克隆仓库以后，仓库中并没有相应的配置文件，进入mass_producer文件夹并使用命令
+
+```bash
+python generate_default_jsons.py
+```
+
+来生成相关的配置文件
 
 
 
@@ -125,20 +133,6 @@ python -u run_pack_maker.py
 
 ## 7 版本控制
 
-现在已经加入自动化版本控制
+现在已经将各种json文件移出了git版本控制，来确保它不会被覆盖
 
-在git pull之后，新的空白json文件会覆盖原本已经配置好的json文件，现在这一问题通过JsonVersionController类解决（./json_version_control.py）。在所有json文件中，添加了两项参数
-
-```json
-"use_former_version": false,
-"@use_former_version": "在git pull之后，本文件会被覆盖，如果需要重拾旧版文件，请将此项设置为true"
-```
-
-如果use_former_version为true，代码将会自动取回上次运行时的json文件并覆盖新的文件，并将新的文件进行保存，所有json文件保存在back_up文件夹中，back_up文件夹不会上传到github仓库中
-
-如果需要手动找回上一版本json，运行
-
-```bash
-python recover_former_jsons.py
-```
 
